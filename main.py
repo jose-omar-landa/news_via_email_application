128c62745f21c8cae778d44613df8bfa54b094d0
Jose Landa <jlanda4@wgu.edu>
2023-11-17 16:36:31 -0600

    added functionality to send emails, added links below descriptions

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+from smtplib import SMTPDataError
+
 import requests
+from send_email import send_email
+
+topic = "tesla"
 
 api_key = "962f77d727f94dc5b71f251f868edb02"
-url = ("https://newsapi.org/v2/everything?q=tesla&from=2023-10-17&"
-       "sortBy=publishedAt&apiKey=962f77d727f94dc5b71f251f868edb02")
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "from=2023-10-17&"
+       "sortBy=publishedAt&"
+       "apiKey=962f77d727f94dc5b71f251f868edb02&"
+       "language=en")
 
 # make request
 request = requests.get(url)
@@ -11,5 +20,13 @@ request = requests.get(url)
 content = request.json()
 
 # access articles titles and description
-for article in content["articles"]:
-    print(article["title"])
+try:
+    body = ""
+    for article in content["articles"][0:20]:
+        if article is not None:
+            body = "Subject: Today's News" + "\n" + body + article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
+
+    body = body.encode("utf-8")
+    send_email(message=body)
+except SMTPDataError:
+    pass

fa2e40a92708c8a8db934f6a05db06160ff90f1b
Jose Landa <121825095+jose-omar-landa@users.noreply.github.com>
2023-11-17 17:19:16 -0600

    Update send_email.py
    updated variables and comments

--- send_email.py
+++ send_email.py
@@ -1,14 +1,16 @@
 import smtplib, ssl
 
-
+# function to send an email via python
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = "jlanda4@wgu.edu"
-    password = "dtixojszzyapnpio"
+    # add your email address for username, and create an app password on your gmail account for the password variable
+    username = "EMAIL_HERE"
+    password = "APP_PASSWORD_HERE"
 
-    receiver = "jlanda4@wgu.edu"
+    # add the recipient email for the receiver variable
+    receiver = "RECEIVER_EMAIL_HERE"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
